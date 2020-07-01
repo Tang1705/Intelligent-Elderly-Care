84acd2f1b2cfc040700b90d81e189372eaf1c4c2
Tang1705 <17301138@bjtu.edu.cn>
2020-07-01 22:57:04 +0800

    add smile detection

---
+++ Class/detection/utils/preprocessor.py
@@ -0,0 +1,29 @@
+import numpy as np
+from PIL import Image
+import numpy as np
+from imageio import imread
+
+def preprocess_input(x, v2=True):
+    x = x.astype('float32')
+    x = x / 255.0
+    if v2:
+        x = x - 0.5
+        x = x * 2.0
+    return x
+
+
+def _imread(image_name):
+        return imread(image_name)
+
+
+def _imresize(image_array, size):
+    return np.array(Image.fromarray(image_array).resize(size))
+        # return imresize(image_array, size)
+
+
+def to_categorical(integer_classes, num_classes=2):
+    integer_classes = np.asarray(integer_classes, dtype='int')
+    num_samples = integer_classes.shape[0]
+    categorical = np.zeros((num_samples, num_classes))
+    categorical[np.arange(num_samples), integer_classes] = 1
+    return categorical
---
+++ Class/detection/video_emotion_color_demo.py
@@ -0,0 +1,105 @@
+from statistics import mode
+
+import cv2
+from keras.models import load_model
+import numpy as np
+
+from utils.datasets import get_labels
+from utils.inference import detect_faces
+from utils.inference import draw_text
+from utils.inference import draw_bounding_box
+from utils.inference import load_detection_model
+from utils.preprocessor import preprocess_input
+
+# parameters for loading data and images
+detection_model_path = './data/data_opencv/res10_300x300_ssd_iter_140000.caffemodel'
+emotion_model_path = 'data/trained_models/emotion_models/fer2013_mini_XCEPTION.102-0.66.hdf5'
+emotion_labels = get_labels('fer2013')
+
+# hyper-parameters for bounding boxes shape
+frame_window = 10
+emotion_offsets = (20, 40)
+
+# loading models
+face_detection = load_detection_model(detection_model_path)
+emotion_classifier = load_model(emotion_model_path, compile=False)
+
+# getting input model shapes for inference
+emotion_target_size = emotion_classifier.input_shape[1:3]
+
+# starting lists for calculating modes
+emotion_window = []
+
+# starting video streaming
+cv2.namedWindow('window_frame')
+video_capture = cv2.VideoCapture(0)
+while True:
+    bgr_image = video_capture.read()[1]
+    gray_image = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2GRAY)
+    rgb_image = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2RGB)
+    faces = detect_faces(face_detection, bgr_image)
+    (h, w) = bgr_image.shape[:2]
+
+    # for face_coordinates in faces:
+    for i in range(0, faces.shape[2]):
+        confidence = faces[0, 0, i, 2]
+        if confidence < 0.5:
+            continue
+
+        box = faces[0, 0, i, 3:7] * np.array([w, h, w, h])
+        (startX, startY, endX, endY) = box.astype("int")
+        x_off, y_off = emotion_offsets
+        x1 = startX - x_off
+        x2 = endX + x_off
+        y1 = startY - y_off
+        y2 = endY + y_off
+
+        face_coordinates=np.array([startX, startY, endX-startX, endY-startY])
+
+        # x1, x2, y1, y2 = apply_offsets(face_coordinates, emotion_offsets)
+        gray_face = gray_image[y1:y2, x1:x2]
+        try:
+            gray_face = cv2.resize(gray_face, (emotion_target_size))
+        except:
+            continue
+
+        gray_face = preprocess_input(gray_face, True)
+        gray_face = np.expand_dims(gray_face, 0)
+        gray_face = np.expand_dims(gray_face, -1)
+        emotion_prediction = emotion_classifier.predict(gray_face)
+        emotion_probability = np.max(emotion_prediction)
+        emotion_label_arg = np.argmax(emotion_prediction)
+        emotion_text = emotion_labels[emotion_label_arg]
+        emotion_window.append(emotion_text)
+
+        if len(emotion_window) > frame_window:
+            emotion_window.pop(0)
+        try:
+            emotion_mode = mode(emotion_window)
+        except:
+            continue
+
+        if emotion_text == 'angry':
+            color = emotion_probability * np.asarray((255, 0, 0))
+        elif emotion_text == 'sad':
+            color = emotion_probability * np.asarray((0, 0, 255))
+        elif emotion_text == 'happy':
+            color = emotion_probability * np.asarray((255, 255, 0))
+        elif emotion_text == 'surprise':
+            color = emotion_probability * np.asarray((0, 255, 255))
+        else:
+            color = emotion_probability * np.asarray((0, 255, 0))
+
+        color = color.astype(int)
+        color = color.tolist()
+
+        draw_bounding_box(face_coordinates, rgb_image, color)
+        draw_text(face_coordinates, rgb_image, emotion_mode,
+                  color, 0, -45, 1, 1)
+
+    bgr_image = cv2.cvtColor(rgb_image, cv2.COLOR_RGB2BGR)
+    cv2.imshow('window_frame', bgr_image)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+video_capture.release()
+cv2.destroyAllWindows()
