d20e9ba9c6df628aa4c5d111e6391c590f6700de
Tang1705 <17301138@bjtu.edu.cn>
2020-07-04 08:16:33 +0800

    extract keypoints by openpose based on gpu

---
+++ Lib/src/openpose/cmake_install.cmake
@@ -0,0 +1,55 @@
+# Install script for directory: D:/BJTU/Python/openpose-master/src/openpose
+
+# Set the install prefix
+if(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/OpenPose")
+endif()
+string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  if(BUILD_TYPE)
+    string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  else()
+    set(CMAKE_INSTALL_CONFIG_NAME "Release")
+  endif()
+  message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
+endif()
+
+# Set the component getting installed.
+if(NOT CMAKE_INSTALL_COMPONENT)
+  if(COMPONENT)
+    message(STATUS "Install component: \"${COMPONENT}\"")
+    set(CMAKE_INSTALL_COMPONENT "${COMPONENT}")
+  else()
+    set(CMAKE_INSTALL_COMPONENT)
+  endif()
+endif()
+
+# Is this installation the result of a crosscompile?
+if(NOT DEFINED CMAKE_CROSSCOMPILING)
+  set(CMAKE_CROSSCOMPILING "FALSE")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for each subdirectory.
+  include("D:/BJTU/Python/openpose-master/build-2017/src/openpose/3d/cmake_install.cmake")
+  include("D:/BJTU/Python/openpose-master/build-2017/src/openpose/calibration/cmake_install.cmake")
+  include("D:/BJTU/Python/openpose-master/build-2017/src/openpose/core/cmake_install.cmake")
+  include("D:/BJTU/Python/openpose-master/build-2017/src/openpose/face/cmake_install.cmake")
+  include("D:/BJTU/Python/openpose-master/build-2017/src/openpose/filestream/cmake_install.cmake")
+  include("D:/BJTU/Python/openpose-master/build-2017/src/openpose/gpu/cmake_install.cmake")
+  include("D:/BJTU/Python/openpose-master/build-2017/src/openpose/gui/cmake_install.cmake")
+  include("D:/BJTU/Python/openpose-master/build-2017/src/openpose/hand/cmake_install.cmake")
+  include("D:/BJTU/Python/openpose-master/build-2017/src/openpose/net/cmake_install.cmake")
+  include("D:/BJTU/Python/openpose-master/build-2017/src/openpose/pose/cmake_install.cmake")
+  include("D:/BJTU/Python/openpose-master/build-2017/src/openpose/producer/cmake_install.cmake")
+  include("D:/BJTU/Python/openpose-master/build-2017/src/openpose/thread/cmake_install.cmake")
+  include("D:/BJTU/Python/openpose-master/build-2017/src/openpose/tracking/cmake_install.cmake")
+  include("D:/BJTU/Python/openpose-master/build-2017/src/openpose/unity/cmake_install.cmake")
+  include("D:/BJTU/Python/openpose-master/build-2017/src/openpose/utilities/cmake_install.cmake")
+  include("D:/BJTU/Python/openpose-master/build-2017/src/openpose/wrapper/cmake_install.cmake")
+
+endif()
+
